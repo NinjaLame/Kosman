d0ab01b15b00ab55f9e15fc2d88f861a56881069
ninjalame <bayu.asrori@gmail.com>
2017-11-30 13:54:29 +0700

    fixing registry animation

--- app/src/main/java/com/example/bayu/kosman/RegistryActivity.java
+++ app/src/main/java/com/example/bayu/kosman/RegistryActivity.java
@@ -1,11 +1,16 @@
 package com.example.bayu.kosman;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.ViewAnimator;
 
@@ -13,27 +18,49 @@ public class RegistryActivity extends AppCompatActivity {
     ViewAnimator regAnimator;
     EditText name,pass,email,phone;
     String sname,spass,semail,sphone;
+    LinearLayout animator;
+    boolean touchable=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
         Toast.makeText(this, "Here Register", Toast.LENGTH_SHORT).show();
         regAnimator = (ViewAnimator)findViewById(R.id.registryAnimator);
-        regAnimator.setInAnimation(this,R.anim.slide_in);
-        regAnimator.setOutAnimation(this,R.anim.slide_out);
+
         name=(EditText)findViewById(R.id.nameReg);
         pass=(EditText)findViewById(R.id.passwordReg);
         email=(EditText)findViewById(R.id.emailReg);
         phone=(EditText)findViewById(R.id.phoneReg);
+        animator= (LinearLayout) findViewById(R.id.animator);
+
+        animator.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()){
+                    case 0:
+                        touchable=true;
+                        animator.setBackgroundColor(Color.rgb(255,220,91));
+
+                        break;
+                     case 1:
+                        animator.setBackgroundColor(0);
+                         break;
+
+                }
+                return true;
+            }
+        });
     }
 
     public void regBackBtn(View view) {
-        regAnimator = (ViewAnimator)findViewById(R.id.registryAnimator);
-        regAnimator.showPrevious();
+        regAnimator.setInAnimation(this,R.anim.back_slide_in);
+        regAnimator.setOutAnimation(this,R.anim.back_slide_out);
+       regAnimator.showPrevious();
     }
 
     public void regNextBtn(View view) {
-        regAnimator = (ViewAnimator)findViewById(R.id.registryAnimator);
+        regAnimator.setInAnimation(this,R.anim.slide_in);
+        regAnimator.setOutAnimation(this,R.anim.slide_out);
         regAnimator.showNext();
     }
 
